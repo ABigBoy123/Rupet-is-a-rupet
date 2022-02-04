2d11043e4e5ee6e4b151e1fdc66366b9ef4afcec
TingTudi <71451877+TingTudi@users.noreply.github.com>
2022-02-04 17:54:57 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -20,48 +20,71 @@ let canvas = document.getElementById("canvas");
 // Drop bombs
 var img = new Image;
 img.src = 'img/explosion.png';
+var gameEnd = false;
 
 document.addEventListener('mousedown', function(e) {
-    var rect = canvas.getBoundingClientRect();
-    let x = e.clientX - rect.left;
-    let y = e.clientY - rect.top;
-    canvasWidth = canvas.offsetWidth;
-    canvasHeight = canvas.offsetHeight;
+    if (!gameEnd) {
+        var rect = canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+        canvasWidth = canvas.offsetWidth;
+        canvasHeight = canvas.offsetHeight;
 
-    
-	
-	// Create an explosion
-	if(x < 0 || y < 0 || x > canvasWidth || y > canvasHeight){
+        
+        
+        // Create an explosion
+        if(x < 0 || y < 0 || x > canvasWidth || y > canvasHeight){
 
-    } else {
-        e = e || window.event;
-        var tag = document.createElement('img');
-        console.log(e);
-        tag.src = img.src;
-        tag.style.position = 'absolute';
-        tag.style.height = '50px';
-        tag.style.width = '50px';
-		tag.style.pointerEvents = "none";
-        tag.id = "temp";
-        tag.style.top = (e.pageY || e.clientY) - 30 + 'px';
-        tag.style.left = (e.pageX || e.clientX) - 25 + 'px';
-        this.body.appendChild(tag);
-       
+        } else {
+            e = e || window.event;
+            var tag = document.createElement('img');
+            console.log(e);
+            tag.src = img.src;
+            tag.style.position = 'absolute';
+            tag.style.height = '50px';
+            tag.style.width = '50px';
+            tag.style.pointerEvents = "none";
+            tag.id = "temp";
+            tag.style.top = (e.pageY || e.clientY) - 30 + 'px';
+            tag.style.left = (e.pageX || e.clientX) - 25 + 'px';
+            this.body.appendChild(tag);
+        
+        }
+        var element = document.getElementById("temp");
+        if (element != 'undefined' && element != null) {
+            setTimeout(function() {
+                document.getElementById("temp").remove();
+            }, 500);
+        }
     }
-    var element = document.getElementById("temp");
-    if (element != 'undefined' && element != null) {
-        setTimeout(function() {
-            document.getElementById("temp").remove();
-        }, 500);
-    }
-    
 }, false)
 
-function updatePoints(){
-	let score = document.getElementById("score").innerHTML;
-	score = parseInt(score) + 1;
-	document.getElementById("score").innerHTML = score;
+// Points
+let allies = ["america", "britain", "france"];
+let axis = ["germany", "italy", "japan"];
+
+function updatePoints(flag){
+    let sliceStart = flag.indexOf("img/") + 4;
+    flag = flag.slice(sliceStart, -4);
+    console.log(flag);
+    if (axis.includes(flag)) {
+        let health = parseInt(document.getElementById("axisHealth").innerHTML);
+        health--;
+        document.getElementById("axisHealth").innerHTML = health;
+    }else if (allies.includes(flag)) {
+        let health = parseInt(document.getElementById("allyHealth").innerHTML);
+        health--;
+        document.getElementById("allyHealth").innerHTML = health;
+    }
 }
+
+canvas.addEventListener("mousedown", function() {
+    if (!gameEnd) {
+        let health = parseInt(document.getElementById("civilianHealth").innerHTML);
+	    health--;
+        document.getElementById("civilianHealth").innerHTML = health;
+    }
+})
 
 // Make flags appear
 var countries = ["america", "britain", "france", "germany", "italy", "japan", "russia"]
@@ -81,8 +104,8 @@ function createFlag() {
     document.body.appendChild(tag);
     
 	//Check if the flag is clicked
-	tag.addEventListener("mousedown", e => {
-		updatePoints();	
+	tag.addEventListener("mousedown", function() {
+		updatePoints(tag.src);	
 	})
 	
     setTimeout(function() {
@@ -91,6 +114,26 @@ function createFlag() {
 }
 
 const interval = setInterval(function() {
-    createFlag();
+    if (!gameEnd) {
+        createFlag();
+    }
 }, (1500 + Math.floor(Math.random() * 1000)));
 
+function endGame() {
+    canvas.style.opacity = 0.25;
+    gameEnd = true
+}
+
+// Check if game ends
+setInterval(function() {
+    let axisHealth = parseInt(document.getElementById("axisHealth").innerHTML);
+    let allyHealth = parseInt(document.getElementById("allyHealth").innerHTML);
+    let civilianHealth = parseInt(document.getElementById("civilianHealth").innerHTML);
+    if (axisHealth <= 0) {
+        endGame();
+    }else if (allyHealth <= 0) {
+        endGame();
+    }else if (civilianHealth <= 0) {
+        endGame();
+    }
+}, 100);
